4db56e765ef689cbcf87b337603f1d20cf42bd2d
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-06 16:33:46 -0300

    Aula 4 - Streams: Trabalhando melhor com coleções

---
+++ Java8/src/ExemploCursos.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+
+class Curso{
+	private String nome;
+	private int alunos;
+	
+	public Curso(String nome, int alunos) {
+		this.nome = nome;
+		this.alunos = alunos;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getAlunos() {
+		return alunos;
+	}
+
+	@Override
+	public String toString() {
+		return "Curso [nome=" + nome + ", alunos=" + alunos + "]";
+	} 
+
+}
+
+public class ExemploCursos {
+	public static void main(String[] args) {
+		
+		List<Curso> cursos = new ArrayList<Curso>();
+		cursos.add(new Curso("Python", 45));
+		cursos.add(new Curso("JavaScript", 150));
+		cursos.add(new Curso("Jaba 8", 113));
+		cursos.add(new Curso("C", 55));
+		
+		//cursos.sort(Comparator.comparing(c -> c.getAlunos()));
+		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
+		//cursos.forEach(c -> System.out.println(c.getNome()));
+		
+		//Imprimir cursos com mais de 100 alunos
+		// Método stream não altera a coleção original
+		int soma = cursos.stream()
+			.filter(c -> c.getAlunos() >= 100)
+			// Imprime
+			//.forEach(c -> System.out.println(c.getNome()));
+			//.map(c -> c.getAlunos())
+			.mapToInt(Curso::getAlunos)
+			.sum();
+		
+		System.out.println(soma);
+		
+	}
+	
+}
