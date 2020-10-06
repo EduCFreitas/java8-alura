5472abc5347cdd6b1a6381e5bfb33681e6030b24
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-06 16:39:08 -0300

    Aula 4 - Inserção de filtro para cursos com mais de 50 alunos

--- Java8/src/ExemploCursos.java
+++ Java8/src/ExemploCursos.java
@@ -33,14 +33,13 @@ public class ExemploCursos {
 		List<Curso> cursos = new ArrayList<Curso>();
 		cursos.add(new Curso("Python", 45));
 		cursos.add(new Curso("JavaScript", 150));
-		cursos.add(new Curso("Jaba 8", 113));
+		cursos.add(new Curso("Java 8", 113));
 		cursos.add(new Curso("C", 55));
 		
 		//cursos.sort(Comparator.comparing(c -> c.getAlunos()));
 		cursos.sort(Comparator.comparingInt(Curso::getAlunos));
 		//cursos.forEach(c -> System.out.println(c.getNome()));
 		
-		//Imprimir cursos com mais de 100 alunos
 		// Método stream não altera a coleção original
 		int soma = cursos.stream()
 			.filter(c -> c.getAlunos() >= 100)
@@ -51,6 +50,11 @@ public class ExemploCursos {
 			.sum();
 		
 		System.out.println(soma);
+		
+		//Imprimir cursos com mais de 500 alunos
+		cursos.stream()
+			.filter(c -> c.getAlunos() > 50)
+			.forEach(c -> System.out.println(c.getNome()));
 		
 	}
 	
