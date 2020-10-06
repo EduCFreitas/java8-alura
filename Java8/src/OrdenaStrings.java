bd5a46963f8e2320b096af1534fcf1f812a235cd
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-06 14:37:32 -0300

    Aula 2 - Ajuste do método sort

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -21,7 +21,7 @@ public class OrdenaStrings {
 		 * palavras.sort((s1, s2) -> { if (s1.length() < s2.length()) { return -1; } if
 		 * (s1.length() > s2.length()) { return 1; } return 0; });
 		 */
-		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+		palavras.sort((s1, s2) -> s1.length() - s2.length());
 
 		System.out.println(palavras);
 
