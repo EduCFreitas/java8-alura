f599a0ccafc39507c816b8d7b8572e9eb70acdfb
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-06 14:33:32 -0300

    Aula 2 - Lambdas e interfaces funcionais

--- Java8/src/OrdenaStrings.java
+++ Java8/src/OrdenaStrings.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -11,45 +9,27 @@ public class OrdenaStrings {
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
-		
-		Comparator<String> comparador = new ComparadorPorTamanho();
-		
-		//Collections.sort(palavras, comparador);
-		palavras.sort(comparador);
+
+		// Interface funcional: possui apenas 1 método abstrato
+		// Lambda funciona quando preciso implementar interface que possui uma interface
+		// funcional
+		// -> identifica lambda
+		// O lambda consegue inferir o tipo do parâmetro (String)
+		// Possibilidade de retirada das chaves por haver apenas um comando
+
+		/*
+		 * palavras.sort((s1, s2) -> { if (s1.length() < s2.length()) { return -1; } if
+		 * (s1.length() > s2.length()) { return 1; } return 0; });
+		 */
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
 		System.out.println(palavras);
-		
-		/*for(String p : palavras) {
-			System.out.println(p);
-		}*/
-		// For each de outra forma
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
+		// A linha de cima corresponde à debaixo
+		palavras.forEach(s -> System.out.println(s));
 
 	}
 
-}
-
-
-class ImprimeNaLinha implements Consumer<String>{
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);	
-	}
-	
-}
-
-class ComparadorPorTamanho implements Comparator<String>{
-
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length()) {
-			return -1;
-		}
-		if(s1.length() > s2.length()) {
-			return 1;
-		}
-		return 0;
-	}
-	
 }
