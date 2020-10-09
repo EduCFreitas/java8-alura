0418f86109f5b9a80d82721ab55eb7868388ed9b
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-09 16:19:46 -0300

    Java Collections - Aula 6: Aplicando Set no modelo

--- gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
+++ gerenciador-de-cursos/src/br/com/alura/TestaAlunos.java
@@ -2,10 +2,8 @@ package br.com.alura;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class TestaAlunos {
 
