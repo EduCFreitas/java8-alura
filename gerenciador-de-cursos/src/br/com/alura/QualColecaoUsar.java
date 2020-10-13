04f893967261ebbb15d95a81432857255ca33944
EduCFreitas <eduardo_freitas_14@yahoo.com.br>
2020-10-13 16:06:00 -0300

    Java Collections - Aula 9: Qual Collection usar

---
+++ gerenciador-de-cursos/src/br/com/alura/QualColecaoUsar.java
@@ -0,0 +1,15 @@
+package br.com.alura;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class QualColecaoUsar {
+	
+	public static void main(String[] args) {
+		Collection<Aluno> alunos = new HashSet<Aluno>();
+		alunos.size();
+		
+		//Set se não precisar se importar com ordem e não puder haver elementos repetidos
+		//List se há a importância com a ordem e podem haver elementos repetidos
+	}
+}
